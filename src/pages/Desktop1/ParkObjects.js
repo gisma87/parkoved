463c6ede0c39a6d32a6861ef45fd7269dc14eee3
Roman Chizhikov <chizhikov.r@gmail.com>
2020-09-26 18:40:52 +0700

    update

---
+++ src/pages/Desktop1/ParkObjects.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './Desktop1.scss'
+import Header from "../../components/Header/Header";
+import SideBar from "../../components/SideBar/SideBar";
+import CardList from "../../components/CardList/CardList";
+import {fp1, fp2, fp3, fp4, fp5, img1, img2, img3, img4} from "../../images/images.js";
+
+function ParkObjects() {
+  const cards = [
+    {img: img1, title: 'Автодром'},
+    {img: img2, title: 'Везунчики'},
+    {img: img3, title: 'Вертолёты'},
+    {img: img4, title: 'Веселое путешествие'}
+  ]
+  const cardsFP = [
+    {img: fp1, title: 'Coffe'},
+    {img: fp2, title: 'Hot food'},
+    {img: fp3, title: 'Бородатый бариста'},
+    {img: fp4, title: 'Мороженое вода'},
+    {img: fp5, title: 'Чайная'}
+  ]
+  return (
+    <div className='ParkObjects'>
+      <Header/>
+      <SideBar/>
+      <div className='ParkObjects__main'>
+        <CardList title='Аттракционы' cards={cards}/>
+        <CardList title='Общепит' cards={cardsFP}/>
+      </div>
+    </div>
+  )
+
+}
+
+export default ParkObjects
