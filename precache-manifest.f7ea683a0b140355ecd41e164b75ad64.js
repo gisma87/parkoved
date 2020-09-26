ec09bee6f13659478edc5980a390ca5db7a35f16
stivman <gisma87@yandex.ru>
2020-09-26 18:24:07 +0700

    Updates

---
+++ precache-manifest.f7ea683a0b140355ecd41e164b75ad64.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bf036ee7c57cc1aeb36484eea256a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50781c2e19a1e4f0aa0a",
+    "url": "/static/css/main.a645971e.chunk.css"
+  },
+  {
+    "revision": "f3c66d71984605f26a11",
+    "url": "/static/js/2.14027f19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14027f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50781c2e19a1e4f0aa0a",
+    "url": "/static/js/main.1af83a60.chunk.js"
+  },
+  {
+    "revision": "7b0e4be1815da7456471",
+    "url": "/static/js/runtime-main.c24f05f8.js"
+  },
+  {
+    "revision": "f2034cc86dd44b7ae8616149eb30f751",
+    "url": "/static/media/IMG_20200924_131552 1.f2034cc8.jpg"
+  },
+  {
+    "revision": "e8b0799d20db69b325faec37a31f6816",
+    "url": "/static/media/IMG_20200924_131951 4.e8b0799d.jpg"
+  },
+  {
+    "revision": "724e6cc73cf6ca5bddfd220dc95cd915",
+    "url": "/static/media/fp1.724e6cc7.jpg"
+  },
+  {
+    "revision": "32e38dbce235b517dd68ad2799770648",
+    "url": "/static/media/fp2.32e38dbc.jpg"
+  },
+  {
+    "revision": "8558f184c318db81b218ede4879ee632",
+    "url": "/static/media/fp3.8558f184.jpg"
+  },
+  {
+    "revision": "38b6c06d7bffc5bfb7424546fb06dbd5",
+    "url": "/static/media/fp4.38b6c06d.jpg"
+  },
+  {
+    "revision": "e6b857e187ead8dd27a9b51dc2b6c338",
+    "url": "/static/media/fp5.e6b857e1.jpg"
+  },
+  {
+    "revision": "d81173561b41b9f431af12c1bc764ff3",
+    "url": "/static/media/везунчики 2.d8117356.jpg"
+  },
+  {
+    "revision": "1859bfe80fceb599ccb5730768ef9930",
+    "url": "/static/media/вертолеты 3.1859bfe8.jpg"
+  }
+]);
